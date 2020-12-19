c39055bf162dd6b8357feda46d4093fe7955ffbc
mvoloskov <hello@miloslav.website>
2020-12-19 13:38:01 +0300

    timing engine

--- dist/butter.js
+++ dist/butter.js
@@ -1,8 +1,71 @@
 var butter = (function () {
     'use strict';
 
-    var index = () => {
-        // console.log('butter')
+    // const handleDCL = () => {
+
+    // }
+
+    // const handleHeavyElements = () => {
+
+    // }
+
+    // const handleWindowOnLoad = () => {
+
+    // }
+
+    var index = (params = {}) => {
+
+        const {
+            msBeforeLoader = 10
+        } = params;
+        
+        const initialReadyState = document.readyState;
+        let currentReadyState = initialReadyState;
+        let isLoaderShown = false;
+
+        console.log(`START`);
+
+        if (initialReadyState === 'loading') {
+            // listen for DOMContentLoaded
+
+            // add a loader timeout
+            const loaderTimeout = setTimeout(() => {
+                if (currentReadyState === 'loading') {
+                    console.log(`${msBeforeLoader}ms have passed, still loading. Should add loader`);
+                    isLoaderShown = true;
+                } else {
+                    console.log(`${msBeforeLoader}ms have passed, already interactive. No loader needed`);
+                }
+            }, msBeforeLoader);
+
+            document.addEventListener('readystatechange', () => {
+                currentReadyState = document.readyState;
+
+                const shouldHideLoader = 
+                    currentReadyState === 'interactive' ||
+                    currentReadyState === 'complete';
+
+                if (isLoaderShown && shouldHideLoader) {
+                    console.log(`ReadyState is ${currentReadyState}, should hide loader`);
+                }
+
+                if (currentReadyState === 'interactive') {
+                    console.log(`ReadyState is interactive, should add listeners to heavy elements`);
+                    clearTimeout(loaderTimeout);
+
+                } else if (currentReadyState === 'complete') {
+                    console.log(`ReadyState is complete, should show everything already. FINISH, destroy everything`);
+                }
+            });
+
+        } else if (initialReadyState === 'interactive') {
+            // add listeners to heavy elements
+
+            console.log(`Initial readyState is interactive, should add listeners to heavy elements`);
+
+        }
+        
+        // we end up here if document is loaded — we should do nothing
     };
 
     return index;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,75 @@
-export default () => {
-    // console.log('butter')
+const setLoader = isShown => {
+    if (isShown) {
+        console.log('Should show loader')
+        return
+    }
+    
+    console.log('Should hide loader')
+}
+
+// const handleDCL = () => {
+
+// }
+
+// const handleHeavyElements = () => {
+
+// }
+
+// const handleWindowOnLoad = () => {
+
+// }
+
+export default (params = {}) => {
+
+    const {
+        msBeforeLoader = 10
+    } = params
+    
+    const initialReadyState = document.readyState
+    let currentReadyState = initialReadyState
+    let isLoaderShown = false
+
+    console.log(`START`)
+
+    if (initialReadyState === 'loading') {
+        // listen for DOMContentLoaded
+
+        // add a loader timeout
+        const loaderTimeout = setTimeout(() => {
+            if (currentReadyState === 'loading') {
+                console.log(`${msBeforeLoader}ms have passed, still loading. Should add loader`)
+                isLoaderShown = true
+            } else {
+                console.log(`${msBeforeLoader}ms have passed, already interactive. No loader needed`)
+            }
+        }, msBeforeLoader)
+
+        document.addEventListener('readystatechange', () => {
+            currentReadyState = document.readyState
+
+            const shouldHideLoader = 
+                currentReadyState === 'interactive' ||
+                currentReadyState === 'complete'
+
+            if (isLoaderShown && shouldHideLoader) {
+                console.log(`ReadyState is ${currentReadyState}, should hide loader`)
+            }
+
+            if (currentReadyState === 'interactive') {
+                console.log(`ReadyState is interactive, should add listeners to heavy elements`)
+                clearTimeout(loaderTimeout)
+
+            } else if (currentReadyState === 'complete') {
+                console.log(`ReadyState is complete, should show everything already. FINISH, destroy everything`)
+            }
+        })
+
+    } else if (initialReadyState === 'interactive') {
+        // add listeners to heavy elements
+
+        console.log(`Initial readyState is interactive, should add listeners to heavy elements`)
+
+    }
+    
+    // we end up here if document is loaded — we should do nothing
 }
